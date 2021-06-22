67caee5047fafc5f73ecef3505361d14269bcc37
Nhan Tran <nhantra@cisco.com>
2021-06-22 14:21:58 -0700

    progress

---
+++ resources/views/partials/home/intro.blade.php
@@ -0,0 +1,37 @@
+<div class="first-page">
+    @include('partials.header') 
+    <div class="card transparent-card" id="firstpage-card">
+        <div class="card-body">
+            <hr class="bar-top-hr"/>
+            <h3 class="card-title blue" style="color:black">Connected. Protective</h5>
+            <p class="card-text grey2" style="color:black">
+                Thirdwayv helps your enterprise realize the full potential 
+                of lOT connectivity. All while neutralizingsecurity threats 
+                that can run ruin the customer experience and your 
+                reputation.
+            </p>
+        </div>
+    </div>
+
+    <div class="follow-container">
+        <div class="">
+            <h5>FOLLOW US</h5>
+        </div>
+        <div class="icons">
+            <i class="bi bi-facebook"></i>
+            <i class="bi bi-twitter"></i>
+            <i class="bi bi-instagram"></i>
+            <i class="bi bi-messenger"></i>
+            <i class="bi bi-linkedin"></i>        
+        </div>
+    </div>
+
+    <div class="row arrows-container">
+        <div class="col arrows">
+            <img src="<?php bloginfo('template_url') ?>/assets/images/arrrow-down-group.svg" alt="" />
+        </div>
+        <div class="col arrows">
+            <img src="<?php bloginfo('template_url') ?>/assets/images/arrow-up-group.svg" alt="" />
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/home/leaders.blade.php
@@ -0,0 +1,76 @@
+<div class="leaders-page">
+    <div style="margin: 10vh 0;">
+        <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-icon.svg" alt="">
+        <h3 class="card-title blue">MEET OUR LEADERS</h3>
+    </div>
+    <div class="row leaders-card-container">
+        <div class="col leaders-card-items">
+            <div class="card transparent-card">
+                <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leader1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">JAMES KAMKE</h5>
+                    <p class="card-text blue">CEO</p>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn leaders-btn">know more</button>
+                        </div>
+                        <div class="col" style="text-align: right;">
+                            <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col leaders-card-items">
+            <div class="card transparent-card">
+                <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leader1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">NABIL WASILY</h5>
+                    <p class="card-text blue">CTO</p>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn leaders-btn">know more</button>
+                        </div>
+                        <div class="col" style="text-align: right;">
+                            <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col leaders-card-items">
+            <div class="card transparent-card">
+                <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leader1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">VINAY GOKHALE</h5>
+                    <p class="card-text blue">VP BUSINESS DEVELOPMENT</p>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn leaders-btn">know more</button>
+                        </div>
+                        <div class="col" style="text-align: right;">
+                            <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col leaders-card-items">
+            <div class="card transparent-card">
+                <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leader1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">ANDREW LENTVORSKI</h5>
+                    <p class="card-text blue">VP ENGINEERING</p>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn leaders-btn">know more</button>
+                        </div>
+                        <div class="col" style="text-align: right;">
+                            <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
