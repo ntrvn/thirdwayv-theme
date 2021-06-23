a4e207052e3e99ef9a936cffe18c0840ece19a01
Nhan Tran <nhantra@cisco.com>
2021-06-23 11:59:28 -0700

    progress

--- resources/views/partials/home/leaders.blade.php
+++ resources/views/partials/home/leaders.blade.php
@@ -10,12 +10,20 @@
                 <div class="card-body">
                     <h5 class="card-title">JAMES KAMKE</h5>
                     <p class="card-text blue">CEO</p>
-                    <div class="row">
-                        <div class="col">
-                            <button class="btn leaders-btn">know more</button>
+                    <div class="leaders-active">
+                        <div class="row">
+                            <div class="col">
+                                <button class="btn leaders-btn">know more</button>
+                                <img src="<?php bloginfo('template_url') ?>/assets/images/thirdPage/third-page-arrow-down.svg" alt="" class="leader-arrows">
+                            </div>
+                            <div class="col" style="text-align: right;">
+                                <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                            </div>
                         </div>
-                        <div class="col" style="text-align: right;">
-                            <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                        <div class="row leaders-know-more" style="">
+                            <p class="small-text grey5">
+                                When it comes to system semiconductor (hardware and software) expertise, Jim’s knowledge runs deep. He spent more than 20 years in marketing, sales, executive management, and product design and engineering, most notably at Motorola. Over the course of his career, he learned the ins and outs of what would become Thirdwayv’s key differentiating point –hardware as the “nucleus of trust,” on which all other security measures are built.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -27,12 +35,20 @@
                 <div class="card-body">
                     <h5 class="card-title">NABIL WASILY</h5>
                     <p class="card-text blue">CTO</p>
-                    <div class="row">
-                        <div class="col">
-                            <button class="btn leaders-btn">know more</button>
+                    <div class="leaders-disabled">
+                        <div class="row">
+                            <div class="col">
+                                <button class="btn leaders-btn">know more</button>
+                                <img src="<?php bloginfo('template_url') ?>/assets/images/thirdPage/third-page-arrow-down.svg" alt="">
+                            </div>
+                            <div class="col" style="text-align: right;">
+                                <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                            </div>
                         </div>
-                        <div class="col" style="text-align: right;">
-                            <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                        <div class="row leaders-know-more" style="padding: 0 15px;">
+                            <p class="small-text grey5">
+                                Nabil  is  an  expert  in  digital  network  architecture design,  wireless  protocols  (LTE,  Wi-Fi,  and  BLE), embedded  SW,  and  security.  He  has  more  than  20  years  of  experience  focused  on  algorithms, semiconductor  design,  and  engineering  management,  which  he  acquired  at  Broadcom,  Newport Media (now owned by Atmel).His obsessive attention to detail not only keeps him up at night, but also sets the tone for the rest of our development team. That’s important because security threats never stop –and neither do we.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -44,12 +60,20 @@
                 <div class="card-body">
                     <h5 class="card-title">VINAY GOKHALE</h5>
                     <p class="card-text blue">VP BUSINESS DEVELOPMENT</p>
-                    <div class="row">
-                        <div class="col">
-                            <button class="btn leaders-btn">know more</button>
+                    <div class="leaders-disabled">
+                        <div class="row">
+                            <div class="col">
+                                <button class="btn leaders-btn">know more</button>
+                                <img src="<?php bloginfo('template_url') ?>/assets/images/thirdPage/third-page-arrow-down.svg" alt="">
+                            </div>
+                            <div class="col" style="text-align: right;">
+                                <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                            </div>
                         </div>
-                        <div class="col" style="text-align: right;">
-                            <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                        <div class="row leaders-know-more" style="padding: 0 15px;">
+                            <p class="small-text grey5">
+                                With  more  than30  years  of  experience  in  the  wireless  industry,  you  could  say  wireless  is  now “hardwired” in Vinay’s brain. Not only has he seen how far the industry has come, but he also sees how far it has yet to go. His deep expertise in cellular, GPS and RFID systems gives him the forward-vision that’s invaluable from both connectivity and security standpoints.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -61,12 +85,20 @@
                 <div class="card-body">
                     <h5 class="card-title">ANDREW LENTVORSKI</h5>
                     <p class="card-text blue">VP ENGINEERING</p>
-                    <div class="row">
-                        <div class="col">
-                            <button class="btn leaders-btn">know more</button>
+                    <div class="leaders-disabled">
+                        <div class="row">
+                            <div class="col">
+                                <button class="btn leaders-btn">know more</button>
+                                <img src="<?php bloginfo('template_url') ?>/assets/images/thirdPage/third-page-arrow-down.svg" alt="">
+                            </div>
+                            <div class="col" style="text-align: right;">
+                                <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                            </div>
                         </div>
-                        <div class="col" style="text-align: right;">
-                            <img src="<?php bloginfo('template_url') ?>/assets/images/leaders/leaders-linkedIn.svg" alt="">
+                        <div class="row leaders-know-more" style="padding: 0 15px;">
+                            <p class="small-text grey5">
+                                A good engineer is naturally curious. Tenacious. And someone who sees patterns and connections that  are  otherwise  hidden  tomost.  Andrew  checks  all  the  boxes.  His  23  years  of  engineering  and management  experience  covers  low  power  microprocessor  and  VLSI  design,  and  embedded  and Cloud Software, with IBM, AMD, DEC and ATI. He has continually proven that, “if he builds it, customers will come.”
+                            </p>
                         </div>
                     </div>
                 </div>
